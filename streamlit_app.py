2a9a46d6bc5c23bea85ef91c17ab292eb89feb14
KumarKadam123 <kumar.kadam@gmail.com>
2025-07-21 01:14:19 +0530

    Update streamlit_app.py
    using search_on in API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #convert the snowflake dataframe into panda dataframe
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients :', my_dataframe, max_selections=5
@@ -33,8 +33,12 @@ if ingredients_list:
     ingredients_string = ''
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == each_fruit, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', each_fruit,' is ', search_on, '.')
+        
         st.subheader(each_fruit + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit)
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
     #st.write(ingredients_string)
